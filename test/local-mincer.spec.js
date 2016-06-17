9e86c9e916f083a15b10fcd3f763e9cab5655c51
MRW Neundorf <m.neundorf@live.de>
2016-06-17 13:35:35 +0200

    local mincer spec

---
+++ test/local-mincer.spec.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var BabelEngine = require('..');
+
+describe('Mincer Babel Engine', function () {
+    it('should load with local mincer', function () {
+        BabelEngine();
+    });
+});
